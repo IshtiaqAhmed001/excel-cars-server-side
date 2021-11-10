4bf12dc4db6dcb3cdd2d8f5f3885e59b889edc6b
IshtiaqAhmed001 <ishtiaq.wasi@gmail.com>
2021-11-10 22:32:55 +0600

    loaded cars from db

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ require('dotenv').config();
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oteh7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-console.log(uri);
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -21,7 +20,16 @@ app.use(express.json());
 async function run() {
     try {
         await client.connect();
-        console.log('db connected successfully')
+
+        const database = client.db("excel_cars");
+        const carsCollection = database.collection("cars");
+
+        // GET CARS 
+        app.get('/cars', async (req, res) => {
+            const cursor = carsCollection.find({});
+            const cars = await cursor.toArray();
+            res.send(cars);
+        })
     }
     finally {
         // await client.close();
